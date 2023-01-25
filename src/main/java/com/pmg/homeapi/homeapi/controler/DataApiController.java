4041a0aed2238f1e59a7862cbd5f7ce5af6894cc
Paco Muñoz <iicc96@hotmail.com>
2023-01-25 17:46:42 +0100

    general changes

--- src/main/java/com/pmg/homeapi/homeapi/controler/DataApiController.java
+++ src/main/java/com/pmg/homeapi/homeapi/controler/DataApiController.java
@@ -9,18 +9,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Controller
-public class HomeApiController implements DataApi {
+public class DataApiController implements DataApi {
 
     @Autowired
     DataCrudRepository dataCrudRepository;
 
     @Override
-    public ResponseEntity<Data> addData(@ApiParam(value = "Add a new data to the database" ,required=true )  @Valid @RequestBody Data data) {
+    public ResponseEntity<Data> addData(@ApiParam(value = "Add a new data to the database" ,required=true )
+                                            @Valid @RequestBody Data data) {
         DataDocument dataDocument = new DataDocument();
         dataDocument.setDate(data.getDate().toString());
         dataDocument.setHumidity(data.getHumidity());
@@ -35,7 +38,16 @@ public class HomeApiController implements DataApi {
         return ResponseEntity.ok(data);
     }
 
-
-
-
+    @Override
+    public ResponseEntity<Data> getLastTempByLoc(@NotNull @ApiParam(value = "the location you want retrieve data from",
+            required = true) @Valid @RequestParam(value = "location", required = true) String location) {
+        List<DataDocument> datos = dataCrudRepository.findAll();
+        DataDocument dataDocument = datos.get(0);
+        Data data = new Data();
+        data.setTemperature(dataDocument.getTemperature());
+        data.setHumidity(dataDocument.getHumidity());
+        data.setLocation(dataDocument.getLocation());
+        data.setDate(dataDocument.getDate());
+        return ResponseEntity.ok(data);
+    }
 }
--- src/main/java/com/pmg/homeapi/homeapi/repository/DataCrudRepository.java
+++ src/main/java/com/pmg/homeapi/homeapi/repository/DataCrudRepository.java
@@ -3,8 +3,12 @@ package com.pmg.homeapi.homeapi.repository;
 import com.pmg.homeapi.homeapi.model.DataDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+
 
 @Repository
 public interface DataCrudRepository extends MongoRepository<DataDocument, String> {
+
+    List<DataDocument> find
 
 }
